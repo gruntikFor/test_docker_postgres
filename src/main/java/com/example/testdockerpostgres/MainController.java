8587c123876c7a91244887e63da13b1fd0b6b405
gruntik <gruntov99@mail.ru>
2022-05-18 21:16:43 +0300

    work with implement apis

--- src/main/java/com/example/testdockerpostgres/MainController.java
+++ src/main/java/com/example/testdockerpostgres/MainController.java
@@ -1,23 +1,19 @@
 package com.example.testdockerpostgres;
 
+import com.example.swaggercodgenfirst.api.*;
 import com.example.testdockerpostgres.entity.Person;
 import com.example.testdockerpostgres.entity.Pet;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 @RestController
-public class MainController {
+public class MainController implements DefaultApi, NameApi {
 
     @Autowired
     PersonRepository repository;
@@ -25,13 +21,8 @@ public class MainController {
     @Autowired
     PetRepository petRepository;
 
-    @Operation(summary = "Add string to db")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Everything ok",
-                    content = {@Content(mediaType = "text/plain; charset=utf-8", schema = @Schema(implementation = String.class))}),
-            @ApiResponse(responseCode = "400", description = "Bad request")})
     @GetMapping("/{name}")
-    public ResponseEntity<String> hello(@Parameter(description = "Name for add into db") @PathVariable(required = false) String name) {
+    public ResponseEntity<String> hello(@PathVariable(required = false) String name) {
         String newName = Objects.requireNonNullElse(name, "igor");
         Person person = new Person();
         person.setName(newName);
@@ -46,13 +37,8 @@ public class MainController {
         return new ResponseEntity<>(retStr, HttpStatus.OK);
     }
 
-    @Operation(summary = "Add pet to db")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Pet added",
-                    content = {@Content(mediaType = "application/json;", schema = @Schema(implementation = Pet.class))}),
-            @ApiResponse(responseCode = "406", description = "Pet already exists")})
     @PostMapping(value = "/pet")
-    public ResponseEntity<Pet> savePet(@RequestBody Pet pet) {
+    public ResponseEntity<?> savePet(@RequestBody Pet pet) {
         if (petRepository.findAllByName(pet.getName()).size() > 0) {
             return new ResponseEntity<>(new Pet(), HttpStatus.NOT_ACCEPTABLE);
         }
@@ -62,13 +48,14 @@ public class MainController {
     }
 
     @PostMapping(value = "/petName", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Object> savePet(@RequestBody HashMap<String, Object> payload) {
+    public ResponseEntity<Object> savePetMap(@RequestBody HashMap<String, Object> payload) {
         System.out.println(payload.get("name"));
         return new ResponseEntity<>(payload.get("name"), HttpStatus.OK);
     }
 
     @GetMapping("/")
-    public String main() {
-        return "just main page";
+    public ResponseEntity<String> main() {
+        return new ResponseEntity<>("just main page", HttpStatus.OK);
     }
+
 }
