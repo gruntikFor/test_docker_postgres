be42b0e12693480410d4ff2bb7744703045da421
gruntik <gruntov99@mail.ru>
2022-05-18 13:42:47 +0300

    fix location

--- src/main/java/com/example/testdockerpostgres/MainController.java
+++ src/main/java/com/example/testdockerpostgres/MainController.java
@@ -8,14 +8,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.HashMap;
 import java.util.Objects;
 
